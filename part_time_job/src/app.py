c3e2dbada6d554a7030cdc0457084b055813a014
Naseem _ <naseemudheene@gmail.com>
2021-02-02 19:31:46 +0530

    Signed-off-by: Naseem _ <naseemudheene@gmail.com>

--- part_time_job/src/app.py
+++ part_time_job/src/app.py
@@ -1,17 +1,31 @@
+import pymysql
 from flask import *
 
+from dbconn import *
 app = Flask(__name__)
-
+con = pymysql.connect(host='localhost',user='root', passwd='',port=3306,db='part_time_job')
+cmd = con.cursor()
 
 @app.route('/')
 def main():
     return render_template('login.html')
 
-@app.route('/login',methods=['post'])
+@app.route('/login',methods=['post','get'])
 def login():
     uname = request.form['textfield']
     passwd = request.form['textfield2']
-    print(uname)
+    cmd.execute("select * from login where username='"+uname+"' and password ='"+passwd+"'")
+    res =cmd.fetchone()
+    print(res)
+    if res is None:
+        return "<script> alert('invalid username or password')</script>"
+    else:
+        if res[3]=='admin':
+            return '''<script>alert('login succesfull admin');window.location='/admin_home' </script>'''
+        else:
+            return "<script> alert('invalid username or password')</script>"
+
+
 
 
 @app.route('/admin_home')
@@ -24,19 +38,53 @@ def manage_job_seeker():
 
 @app.route('/verify_employer')
 def verify_employer():
-    return render_template('Admin/verify_employer.html')
+    cmd.execute("SELECT `employer`.*,`login`.* FROM `login` JOIN `employer` ON `employer`.`login_id`=`login`.`id` WHERE `login`.`type`='pending'")
+    result =cmd.fetchall()
+
+    return render_template('Admin/verify_employer.html',val=result)
+
+@app.route('/accept_employer',methods=['get','post'])
+def accept_employer():
+    id =request.args.get('id')
+    cmd.execute("update login set login.type='employer' where id='"+str(id)+"'")
+    con.commit()
+    return '''<script>alert('Employer Accepted');window.location='/verify_employer'</script>'''
+
+@app.route('/reject_employer',methods=['get','post'])
+def reject_employer():
+    id =request.args.get('id')
+    cmd.execute("delete from login where id='"+str(id)+"'")
+    cmd.execute("delete from employer where login_id='"+str(id)+"'")
+    con.commit()
+    return '''<script>alert('Employer Deleted');window.location='/verify_employer'</script>'''
+
+
+@app.route('/view_job_req')
+def view_job_req():
+    cmd.execute("select * from job")
+    result = cmd.fetchall()
+
+    return render_template('Admin/view_job_req.html',val=result)
+
+
 
 @app.route('/complaint_admin')
 def complaint_admin():
     return render_template('Admin/view_complaint.html')
 
+@app.route('/reply_complaint_admin')
+def reply_complaint_admin():
+    return render_template('Admin/reply_complaint.html')
+
 @app.route('/feedback_admin')
 def feedback_admin():
     return render_template('Admin/view_feedback.html')
 
-@app.route('/view_job_req')
-def view_job_req():
-    return render_template('Admin/view_job_req.html')
+@app.route('/reply_feedback_admin')
+def reply_feedback_admin():
+    return render_template('Admin/reply_feedback.html')
+
+
 
 @app.route('/employer_register')
 def employer_register():
--- part_time_job/src/dbconn.py
+++ part_time_job/src/dbconn.py
@@ -1,10 +1,26 @@
-import pymysql
-
-def iud(qry,vals):
-    con = pymysql.connect(host='localhost',
-                      user='root', passwd='',
-                      port=3306,db='part_time_job')
-    cmd = con.cursor()
-    cmd.execute(qry,vals)
-    con.commit()
-    con.close()
+# import pymysql
+#
+# def iud(qry,vals):
+#     con = pymysql.connect(host='localhost',user='root', passwd='',port=3306,db='part_time_job')
+#     cmd = con.cursor()
+#     cmd.execute(qry,vals)
+#     con.commit()
+#     con.close()
+#
+# def selectone(qry):
+#     con =pymysql.connect(host='localhost',user='root',passwd='',port=3306,db='part_time_job')
+#     cmd = con.cursor()
+#     cmd.execute(qry)
+#     res=selectone(qry)
+#     con.commit()
+#     con.close()
+#     return res
+#
+# def selectall(qry):
+#     con = pymysql.connect(host='localhost', user='root', passwd='', port=3306, db='part_time_job')
+#     cmd = con.cursor()
+#     res=selectall(qry)
+#     cmd.commit()
+#     con.close()
+#     return res
+#
